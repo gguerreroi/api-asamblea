79f73dcb9eda4ce39fcdf4aa3e13dc5bfa5f2036
gguerreroi <gguerrero@sonivitac.com>
2021-08-10 23:48:37 -0600

    First Commit

--- src/controllers/Votaciones.controller.ts
+++ src/controllers/Votaciones.controller.ts
@@ -27,6 +27,75 @@ export async function votaciones(req: Request, res: Response) {
     }
 }
 
+export async function IniciarVotacion(req: Request, res: Response){
+    let Connection = null
+    const {codvotaciones} = req.params;
+    try {
+        Connection = await getConnection()
+        const sp = await Connection.request();
+
+        sp.input("codvotacion", mssql.Int, codvotaciones)
+        sp.output("CodMsj", mssql.Int)
+        sp.output("StrMsj", mssql.VarChar(400))
+        sp.execute('Votaciones.sp_iniciar', (error: any, results: any) => {
+            if (!error)
+                return res.status(200).send(JsonOut('1', 'Lista de Opciones', results.recordset));
+
+            return res.status(200).send(JsonOut('0', 'Sin Datos'));
+        })
+    } catch (e: any) {
+        const {Code, Message} = Connection;
+        return res.status(200).send(JsonOut(Code, Message, e));
+    }
+}
+
+export async function DetenerVotacion(req: Request, res: Response){
+    let Connection = null
+    const {codvotaciones} = req.params;
+    try {
+        Connection = await getConnection()
+        const sp = await Connection.request();
+
+        sp.input("codvotacion", mssql.Int, codvotaciones)
+        sp.output("CodMsj", mssql.Int)
+        sp.output("StrMsj", mssql.VarChar(400))
+        sp.execute('Votaciones.sp_detener', (error: any, results: any) => {
+            if (!error)
+                return res.status(200).send(JsonOut('1', 'Lista de Opciones', results.recordset));
+
+            return res.status(200).send(JsonOut('0', 'Sin Datos'));
+        })
+    } catch (e: any) {
+        const {Code, Message} = Connection;
+        return res.status(200).send(JsonOut(Code, Message, e));
+    }
+}
+
+export async function iucore(req: Request, res: Response){
+    let Connection = null
+    const {codvotaciones} = req.params;
+    const {strtitulo, strdescripcion} = req.body;
+    try {
+        Connection = await getConnection()
+        const sp = await Connection.request();
+
+        sp.input("codvotacion", mssql.Int, codvotaciones)
+        sp.input("strtitulo", mssql.VarChar(200), strtitulo)
+        sp.input("strdescripcion", mssql.VarChar(200), strdescripcion)
+        sp.output("CodMsj", mssql.Int)
+        sp.output("StrMsj", mssql.VarChar(400))
+        sp.execute('Votaciones.sp_iucore', (error: any, results: any) => {
+            if (!error)
+                return res.status(200).send(JsonOut('1', 'Lista de Opciones', results.recordset));
+
+            return res.status(200).send(JsonOut('0', 'Sin Datos'));
+        })
+    } catch (e: any) {
+        const {Code, Message} = Connection;
+        return res.status(200).send(JsonOut(Code, Message, e));
+    }
+}
+
 export async function opciones(req: Request, res: Response) {
     let Connection = null
     try {
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ import * as Seguridad from "../controllers/Seguridad.controller"
 import * as Votacion from "../controllers/Votaciones.controller"
 import * as Asamblea from "../controllers/Asamblea.controller"
 
+
 const r = Router();
 
 r.get("/asociados/login", Seguridad.asociados_login)
@@ -10,6 +11,8 @@ r.post("/asociados/login", Seguridad.asociados_asistencia)
 r.get("/asociados/votaciones/:codcliente", Votacion.votaciones)
 r.get("/asociados/opciones/", Votacion.opciones)
 r.get("/votaciones", Votacion.votaciones)
+r.post("/votaciones/:codvotacion", Votacion.IniciarVotacion)
+r.put("/votaciones/:codvotacion", Votacion.DetenerVotacion)
 r.get("/admin/login", Seguridad.admin_login)
 r.get("/asamblea/resultados", Asamblea.resultados)
 r.get("/asamblea/asistencia", Asamblea.asistencia)
